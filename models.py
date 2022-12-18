02867524dc9397c8c2d270670f64f0688a6541de
pgil256 <113308245+pgil256@users.noreply.github.com>
2022-12-17 20:47:16 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,18 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+
+    __table__ = 'users'
+
+    id == db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable = False)
+    last_name = db.Column(db.Text, nullable = False)
+    image_url = db.Column(db.Text, nullable=False)
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
